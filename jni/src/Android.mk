2bc98d1f334df06f166646cf85c809a96a884039
psi <ledyba@users.sourceforge.jp>
2013-08-27 04:42:00 +0900

    enum sources

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -7,10 +7,44 @@ LOCAL_MODULE := main
 SDL_PATH := ../SDL
 SDL_mixer_PATH := ../SDL_mixer
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/$(SDL_mixer_PATH)
+LOCAL_C_INCLUDES :=\
+	$(LOCAL_PATH)/$(SDL_PATH)/include\
+	$(LOCAL_PATH)/$(SDL_mixer_PATH)\
+	$(LOCAL_PATH)/../bulletml\
+	$(LOCAL_PATH)/../bulletml/src\
+	$(LOCAL_PATH)/../bulletml/src/boost
 
 # Add your application source files here...
-LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c $(shell find $(LOCAL_PATH) -name *.c | cut -c 9-)
+LOCAL_SRC_FILES := \
+./soundmanager.c     \
+./ship.c             \
+./shot.c             \
+./letterrender.c     \
+./barragemanager.cc  \
+./vector.c           \
+./rand.c             \
+./attractmanager.c   \
+./background.c       \
+./bonus.c            \
+./screen.c           \
+./clrtbl.c           \
+./foecommand.cc      \
+./foe.cc             \
+./frag.c             \
+./noiz2sa.c          \
+./degutil.c          \
+../bulletml/src/formula-variables.cpp           \
+../bulletml/src/bulletmlparser-tinyxml.cpp      \
+../bulletml/src/bulletmlrunnerimpl.cpp          \
+../bulletml/src/calc.cpp                        \
+../bulletml/src/bulletmlparser.cpp              \
+../bulletml/src/bulletmltree.cpp                \
+../bulletml/src/tinyxml/tinystr.cpp             \
+../bulletml/src/tinyxml/tinyxml.cpp             \
+../bulletml/src/tinyxml/tinyxmlerror.cpp        \
+../bulletml/src/tinyxml/tinyxmlparser.cpp       \
+../bulletml/src/bulletmlrunner.cpp              \
+$(SDL_PATH)/src/main/android/SDL_android_main.c
 
 LOCAL_SHARED_LIBRARIES := SDL2 SDL_mixer
 LOCAL_STATIC_LIBRARIES := android_support
